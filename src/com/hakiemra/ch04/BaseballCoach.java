4cdbd475929ad3908bb479b8620e63d8c589e02a
hakiemra <hakiem.ra@gmail.com>
2021-02-20 21:41:29 +0700

    Basic Java Application

---
+++ src/com/hakiemra/ch04/BaseballCoach.java
@@ -0,0 +1,7 @@
+package com.hakiemra.ch04;
+
+public class BaseballCoach {
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on Baseball practice";
+	}
+}
---
+++ src/com/hakiemra/ch04/MyApp.java
@@ -0,0 +1,13 @@
+package com.hakiemra.ch04;
+
+public class MyApp { // Run as Java Application
+	
+	public static void main(String[] args) {
+		// Create Object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		// Use Methods of Object
+		System.out.println(theCoach.getDailyWorkout());
+		
+	}
+}
