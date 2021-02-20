5d1f9b12beb2df74c00f2b1af3d0e7282460d9f1
hakiemra <hakiem.ra@gmail.com>
2021-02-21 00:02:09 +0700

    Configuring Bean Using XML Configuration Method

--- src/com/hakiemra/ch04/MyApp.java
+++ src/com/hakiemra/ch04/MyApp.java
@@ -1,13 +1,24 @@
 package com.hakiemra.ch04;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp { // Run as Java Application
 	
 	public static void main(String[] args) {
+		/*
 		// Create Object
 		Coach theCoach = new TrackCoach(); // Hardcode, Not the right way to do it
+		*/
+		
+		// Start New Context by Getting XML Config Files
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Retrieve Bean Object
+		Coach theCoach = context.getBean("coach", Coach.class); // Must be same with beans.id in XML config file
 		
 		// Use Methods of Object
 		System.out.println(theCoach.getDailyWorkout());
 		
+		context.close();
 	}
 }
