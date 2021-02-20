b0255fdd61632ffd85f427b1d3058dea2e176256
hakiemra <hakiem.ra@gmail.com>
2021-02-20 23:27:19 +0700

    Hardcode, Not the right way to do it

--- src/com/hakiemra/ch04/MyApp.java
+++ src/com/hakiemra/ch04/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp { // Run as Java Application
 	
 	public static void main(String[] args) {
 		// Create Object
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach(); // Hardcode, Not the right way to do it
 		
 		// Use Methods of Object
 		System.out.println(theCoach.getDailyWorkout());
---
+++ src/com/hakiemra/ch04/TrackCoach.java
@@ -0,0 +1,8 @@
+package com.hakiemra.ch04;
+
+public class TrackCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "Run 5k a day";
+	}
+}
