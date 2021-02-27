14c95843c6e3518569a72fb50712a3c4ceb44826
hakiemra <hakiem.ra@gmail.com>
2021-02-27 15:49:35 +0700

    When 2 @Component with Same 'implements' Interface Exists - Throw an ERROR

---
+++ src/com/hakiemra/ch08/RandomFortuneService.java
@@ -0,0 +1,14 @@
+package com.hakiemra.ch08;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+	@Override
+	public String getFortune() {
+		return "This is your random day!";
+	}
+}
+// Will throw an ERROR
+// No qualifying bean of type 'com.hakiemra.ch08.FortuneService' available
+// : expected single matching bean but found 2: happyFortuneService,randomFortuneService
