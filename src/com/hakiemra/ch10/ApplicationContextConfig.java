81d0a1eb0e6d4cc13bc24d2fd17edd0b2f1d212f
hakiemra <hakiem.ra@gmail.com>
2021-03-04 00:15:51 +0700

    Using @Bean inside @Configuration instead of @Component with @ComponentScan

--- src/com/hakiemra/ch10/ApplicationContextConfig.java
+++ src/com/hakiemra/ch10/ApplicationContextConfig.java
@@ -1,10 +1,19 @@
 package com.hakiemra.ch10;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:com/hakiemra/ch10/MyApp.properties")
 public class ApplicationContextConfig {
+    @Bean
+    public Coach cricketCoach() {
+        return new CricketCoach();
+    }
 
+    @Bean
+    public FortuneService randomFortuneService() {
+        return new RandomFortuneService();
+    }
 }
